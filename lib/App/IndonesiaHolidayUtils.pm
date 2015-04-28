d53c86680edc5069e21642f3d2072dbd06825f72
perlancar (@pc-office) <perlancar@gmail.com>
2015-04-28 20:33:46 +0700

    Initial commit

---
+++ lib/App/IndonesiaHolidayUtils.pm
@@ -0,0 +1,20 @@
+package App::ListIdHolidays;
+
+use 5.010001;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: List Indonesian holidays
+
+=head1 SYNOPSIS
+
+This distribution contains several CLI's related to Indonesian holidays:
+
+# EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Calendar::Indonesia::Holiday>
